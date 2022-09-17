a204359886616d424f1db901195cc2c4d28d53f1
Black-Boy <108489528+X-R-404@users.noreply.github.com>
2022-09-17 12:30:08 +0600

    XR.py

---
+++ XR.py
@@ -0,0 +1,8 @@
+import os, platform
+os.system('xdg-open https://www.facebook.com/X.R.404')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import bxi
+else:
+    exit('\033[1;32m [Sorry Not Support Your Phone]')
+exit()
